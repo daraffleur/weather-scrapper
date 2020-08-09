119796269e218304f00c07d14c7056a97c7aa2ee
Kunal <knkillekar@gmail.com>
2020-08-09 16:35:18 +0530

    modified weather_scraper

--- weather.py
+++ weather.py
@@ -2,23 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 page = requests.get("https://www.bbc.com/weather/1275339")
-soup = BeautifulSoup(page.content, 'html.parser')
-fortheen_day = soup.find(id="wr-forecast")
-loc=soup.find(class_="wr-c-location")
-print(loc)
-doc=fortheen_day.find_all(class_='wr-js-day')
-tonight=doc[0]
-print(tonight.prettify())
-location=loc.find(id="wr-location-name-id").get_text()
-period=tonight.find(class_="wr-day__title").get_text()
-temp = tonight.find(class_="wr-day__temperature").get_text()
-short_desc = tonight.find(class_="wr-day__details__weather-type-description").get_text()
+print(page)
+soup = BeautifulSoup(page.text, 'html.parser')
 
-print(location)
-print(period)
-print(temp)
-print(short_desc)
+weather=soup.find(class_="wr-day-carousel__scrollable")
 
+days=weather.find_all('li')
+print(len(days))
 
 # Open writer with name
 file_name = "weather.csv"
@@ -28,17 +18,21 @@ f = csv.writer(open(file_name, 'w', newline=''))
 # write a new row as a header
 f.writerow(['Day', 'Description', 'Temperature'])
 
-for fortheen_day in doc:
 
-    period_tags = fortheen_day.select(".wr-day__title")
-    periods = [pt.get_text() for pt in period_tags]
-    temps = [t.get_text() for t in fortheen_day.select(".wr-day__temperature")]
-    short_descs = [sd.get_text() for sd in fortheen_day.select(".wr-day__details__weather-type-description")]
+for weather in days:
 
-print('periods', periods)
-print('temps',temps)
-print('short_descs', short_descs)
+    day=weather.find(class_="wr-date").get_text()
+    #print(day)
+    desc=weather.find(class_="wr-day__weather-type-description-container").get_text()
+    #print(desc)
+    temp=weather.find(class_="wr-day-temperature").get_text()
+    #print(temp)
 
-print('Writing rows')
+
+    print('day', day)
+    print('desc', desc)
+    print('temp', temp)
+
+    print('Writing rows')
     # add the information as a row into the csv table
-f.writerow([periods, short_descs, temps])
+    f.writerow([day, desc, temp])
